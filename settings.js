12a49db3eecd2b39229e9556ff9a249e2626cc08
Aloneboytech <aloneboytech88@gmail.com>
2025-03-21 18:25:29 -0300

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,52 @@
+
+
+//══════════════════════════════════════════════════════════════════════════════════════════════════════//
+//*
+//  * @project_name : INCONNU-MD
+//  * @author : inconnu boy
+//  * @youtube : https://www.youtube.com/@InconnuBoyTech
+//  * @description : INCONNU-MD ,A Multi-functional whatsapp user bot.
+//*
+//*
+//base by inconnu boy
+//GitHub: @Aloneboytech
+//WhatsApp: +554488122687
+//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@InconnuBoyTech
+//   * Created By Github: Aloneboytech.
+//   * Credit To inconnu boy
+//   * © 2025 INCONNU-MD-V7.
+// ⛥┌┤
+// */
+
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+
+SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
+PREFIX: process.env.PREFIX || '.' ,
+PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
+SUDO: process.env.SUDO === undefined ? '554488122687' : process.env.SUDO,
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
+MODE: process.env.MODE === undefined ?"public" : process.env.MODE,
+AUTO_VOICE: process.env.AUTO_VOICE === undefined ? "false" : process.env.AUTO_VOICE,
+ANTI_BAD: process.env.ANTI_BAD === undefined ? "false" : process.env.ANTI_BAD,
+ANTI_LINK: process.env.ANTI_LINK === undefined ? "true" : process.env.ANTI_LINK,
+ANTI_CALL: process.env.ANTI_CALL === undefined ? "true" : process.env.ANTI_CALL,    
+DELETEMSGSENDTO : process.env.DELETEMSGSENDTO === undefined ? '' : process.env.DELETEMSGSENDTO,
+ANTI_DELETE : process.env.ANTI_DELETE === undefined ? 'true' : process.env.ANTI_DELETE,
+ANTI_BOT: process.env.ANTI_BOT === undefined ? "true" : process.env.ANTI_BOT,
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE === undefined ? "false" : process.env.ALWAYS_ONLINE,
+READ_CMD: process.env.READ_CMD === undefined ? "true" : process.env.READ_CMD,
+RECORDING: process.env.RECORDING === undefined ? "true" : process.env.RECORDING,
+AI_CHAT: process.env.AI_CHAT === undefined ? "false" : process.env.AI_CHAT,
+AUTO_REACT: process.env.AUTO_REACT === undefined ? "false" : process.env.AUTO_REACT,
+AUTO_TYPING: process.env.AUTO_TYPING === undefined ? 'false' : process.env.AUTO_TYPING,
+AUTO_BIO: process.env.AUTO_BIO === undefined ? 'true' : process.env.AUTO_BIO,
+ANTI_STICKER: process.env.ANTI_STICKER === undefined ? 'false' : process.env.ANTI_STICKER,
+ANTI_PHOTO: process.env.ANTI_PHOTO === undefined ? 'false' : process.env.ANTI_PHOTO,
+MOROCCO_BLOCK: process.env.MOROCCO_BLOCK === undefined ? 'false' : process.env.MOROCCO_BLOCK,
+POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://inconnu:your_password@your_host/inconnu_md_db' : process.env.POSTGRESQL_URL,
+};
